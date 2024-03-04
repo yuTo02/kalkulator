8cddd2877e7da92f2c9b4c269b4fc7aa2fc711e9
yuTo02 <smtpses4@gmail.com>
2024-03-04 22:20:11 +0700

    feat:add main program

---
+++ kalkulator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class kalkulator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // inisialisasi objek Scanner untuk membuat user berinteraksi dalam
+                                                // memasukan angka
+        // deklarasi program kalkulator
+        System.out.println("==========KALKULATOR SEDERHANA==========");
+       
+
+    }
+}
